9b3aa5ea9e90ac4d89cc1f0bf6412da690c444b0
Kevin Cheng <09chengk@gmail.com>
2016-01-27 21:51:05 -0800

    fixes .gitignore file, working on saving long and lat

--- client/javascripts/factories.js
+++ client/javascripts/factories.js
@@ -1,10 +1,11 @@
 // I think this should actually be a factory? Then we can use the data
 // we get from the get request inside the controllers?
 angular.module('foodTruck')
-    .factory('foodTruckCoordsCtrl', ['$http', function($http){
-        $http.get('https://data.sfgov.org/resource/rqzj-sfat.json')
-            .success(function(data) {
-                var coords = this.data;
-                console.log(coords);
-            });
-    }]);
+    .factory('FoodTruckCoords', ['$http', function($http){
+        this.getFoodTruckInfo = function() {
+            $http.get('https://data.sfgov.org/resource/rqzj-sfat.json')
+                .success(function(data) {
+                    console.log(data);
+                });
+            };
+        }]);
