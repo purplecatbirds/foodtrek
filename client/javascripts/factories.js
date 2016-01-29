5484a1e9a09f56f74a8b3e736f3d6047d78b51a0
Kevin Cheng <09chengk@gmail.com>
2016-01-29 15:20:52 -0800

    refactored FoodTruckCoords a bit

--- client/javascripts/factories.js
+++ client/javascripts/factories.js
@@ -2,10 +2,10 @@
 // we get from the get request inside the controllers?
 angular.module('foodTruck')
     .factory('FoodTruckCoords', ['$http', function($http){
-        this.getFoodTruckInfo = function() {
-            $http.get('https://data.sfgov.org/resource/rqzj-sfat.json')
-                .success(function(data) {
-                    console.log(data);
-                });
+        var truckInfo = {};
+        truckInfo.getFoodTruckInfo = function() {
+            return $http.get('https://data.sfgov.org/resource/rqzj-sfat.json')
+                };
+                return truckInfo;
             };
-        }]);
+        ]);
