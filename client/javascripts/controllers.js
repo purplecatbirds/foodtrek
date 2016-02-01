5434013ff5f9cc59d435f68f4d44e0e444297634
Kevin Cheng <09chengk@gmail.com>
2016-02-01 06:59:10 -0800

    stores user inputted address

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -3,22 +3,21 @@ angular.module('foodTruck')
         var vm = this;
         var longAndLats = [];
         // gets truck info using function from factory
-        // all the long and lats are the same, is this the right data?
         function init() {
             FoodTruckCoords.getFoodTruckInfo()
                 .success(function(data) {
                     vm.truckInfo = data;
                     for (var i = 0; i < data.length; i++) {
                     if (data[i]['location']) {
-                        longAndLats.push([data[i]['location']['latitude'], data[i]['location']['longitude']])
+                        longAndLats.push([data[i]['applicant'], data[i]['location']['latitude'], data[i]['location']['longitude']])
                         }
                     }
+                    console.log(data)
                     console.log(longAndLats);
                 })
             }
 
-        function submit() {
-            var address = $scope.address;
+        $scope.submit = function(address) {
             console.log(address)
         }
         init();
--- client/javascripts/factories.js
+++ client/javascripts/factories.js
@@ -1,7 +1,7 @@
 // I think this should actually be a factory? Then we can use the data
 // we get from the get request inside the controllers?
 angular.module('foodTruck')
-    .factory('FoodTruckCoords', ['$http', function($http){
+    .factory('FoodTruckCoords', ['$http', function($http) {
         var truckInfo = {};
         truckInfo.getFoodTruckInfo = function() {
             return $http.get('https://data.sfgov.org/resource/rqzj-sfat.json')
