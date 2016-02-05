da81e3bb50d2b96ccd53f7fe4fc1bb3ba3c65e18
Casandra Silva <silvacasandra@gmail.com>
2016-02-04 22:56:54 -0800

    modifies coordsCtrl into a constructor; comments out mapCoordCtrl
    dfssdf

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -1,21 +1,24 @@
 angular.module('foodTruck')
-    .controller('coordsCtrl', ['FoodTruckCoords', '$scope', function(FoodTruckCoords, $scope){
-        var vm = this;
-        var longAndLats = [];
-        // gets truck info using function from factory
-        function init() {
-            FoodTruckCoords.getFoodTruckInfo()
-                .success(function(data) {
-                    vm.truckInfo = data;
-                    for (var i = 0; i < data.length; i++) {
-                    if (data[i]['location']) {
-                        longAndLats.push([data[i]['applicant'], data[i]['location']['latitude'], data[i]['location']['longitude']])
-                        }
-                    }
-                    console.log(data)
-                    console.log(longAndLats);
-                })
+  .controller('coordsCtrl', coordsCtrl);
+
+  coordsCtrl.$inject = ['FoodTruckCoords'];
+  function coordsCtrl (FoodTruckCoords) {
+    vm = this;
+    var longAndLats = [];
+  // gets truck info using function from factory
+    function init() {
+      FoodTruckCoords.getFoodTruckInfo()
+        .success(function(data) {
+          vm.truckInfo = data;
+          for (var i = 0; i < data.length; i++) {
+            if (data[i]['location']) {
+              longAndLats.push([data[i]['applicant'], data[i]['location']['latitude'], data[i]['location']['longitude']])
             }
+          }
+          console.log(data)
+          console.log(longAndLats);
+        })
+    }
 
         //should this be in a factory?
         vm.submit = function() {
@@ -24,14 +27,17 @@ angular.module('foodTruck')
             console.log(saveAddress)
         }
         init();
-    }])
+  }
 
     // need to put in a controller to push the longs and lats that were stored in the init fn
-    .controller('mapCoordCtrl', ['FoodTruckCoord', function(FoodTruckCoord){
-        for (var i = 0; i < longAndLats.length; i++) {
-            
-        }
-    }])
+
+  //commenting out for now -CS
+
+  // .controller('mapCoordCtrl', ['FoodTruckCoord', function(FoodTruckCoord){
+  //   for (var i = 0; i < longAndLats.length; i++) {
+
+  //   }
+  // }])
 
 
 
