78ee31cc9c6296c51662cd5072a9494adc263fba
Kevin Cheng <09chengk@gmail.com>
2016-02-16 14:52:59 -0800

    refactored map view

---
+++ client/javascripts/map.js
@@ -0,0 +1,11 @@
+$(function(){
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng({lat: 37.7741, lng: -122.4312}),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  setTimeout(function() {
+    $('.map-body').append('#map').css('margin-top', '0px').get(0);
+  }, 500)
+})
